7b029d6207cf090a6a006710cd169388604441ee
Live session user <sprabha1814@gmail.com>
2017-09-16 07:52:01 +0000

    commit

--- app/data/Message.java
+++ app/data/Message.java
@@ -1,4 +1,10 @@
 package data;
 
 public class Message {
+    public String text;
+    public FeedResponse feedResponse;
+    public Message.Sender sender;
+    public enum Sender{
+        BOT,USER;
+    }
 }
--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -2,4 +2,23 @@ var app = angular .module('chatApp',['ngMaterial']);
 
 app.controller('chatController',function ($scope) {
 
+	$scope.message = [
+	{
+		sender:"USER",
+		text:"Hello World",
+	     time:"11:00 am"},
+		{
+			sender:"BOT",
+			text:"Hey!!",
+			time:"12:00 am"
+
+		}];
+
+var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);
+   };
+
+
 });
